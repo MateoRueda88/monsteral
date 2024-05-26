07f93141745e5ecd62fbb8b7b7d2439defdccf96
Mateo Rueda <mateo.rueda@genstudents.org>
2024-05-26 18:30:14 -0500

    Agregando Pagina Contact

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,24 +1,25 @@
+const formulario = document.getElementById('form')
+const nombre = document.getElementById('nameError')
+formulario.addEventListener('click', (event) =>{
+  let valorInput = event.target.value;
+  console.log(valorInput)
+})
+
+//  document.getElementById('form').addEventListener('submit', function(event) {
+//    event.preventDefault(); // Previene el envío del formulario
+
+//    const email = document.getElementById('email');
 
 
+// email.addEventListener("input", function (event) {
+//   if (email.validity.typeMismatch) {
+//     email.setCustomValidity(
+//       "¡Se esperaba una dirección de correo electrónico!",
+//     );
+//   } else {
+//     email.setCustomValidity("");
+//   }
+// });
 
 
-
-
- document.getElementById('form').addEventListener('submit', function(event) {
-   event.preventDefault(); // Previene el envío del formulario
-
-   const email = document.getElementById('email');
-
-
-email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity(
-      "¡Se esperaba una dirección de correo electrónico!",
-    );
-  } else {
-    email.setCustomValidity("");
-  }
-});
-
-
- });
+//  });
