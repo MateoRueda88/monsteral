18e6a503eb79f4f5dd52c9d558c9569d4b4ac6dc
Mateo Rueda <mateo.rueda@genstudents.org>
2024-06-17 15:33:04 -0500

    modificando estilos de SweetAlerts de contacto y registro

--- src/js/contact.js
+++ src/js/contact.js
@@ -13,7 +13,23 @@ function longitudInput(event){
   let valorInput = event.target.value;
 let longitudInputNombre = valorInput.length;
   if(longitudInputNombre < 3){
-errName.textContent = ("Ingresa mas caracteres")
+    Swal.fire({
+      title: "Ingrese mas caracteres",
+      showConfirmButton: false,
+      background: '#005c39',
+      icon: 'info',
+      timer: '1000',
+      position: 'center',
+      backdrop: true,
+      color: '#939535',
+      allowOutsideClick: true,
+      toost: true,
+      showConfirmButton: false,
+      customClass:{
+          title: 'titulo-password'
+      }
+});
+// errName.textContent = ("Ingresa mas caracteres")
   }else{
     errName.textContent = ("✔")
   }
@@ -27,7 +43,22 @@ phone.addEventListener('change', (event) => {
 function tipoDato(event) {
   let tipoDate = event.target.value;
   if (isNaN(tipoDate)) {
-      errPhone.textContent = "Caracter no válido";
+    Swal.fire({
+      title: "Ingrese campo valido",
+      showConfirmButton: false,
+      background: '#52c096',
+      icon: 'warning',
+      timer: '7000',
+      position: 'center',
+      backdrop: true,
+      color: '#939535',
+      allowOutsideClick: true,
+      toost: true,
+      showConfirmButton: false,
+      customClass:{
+          title: 'titulo-password'
+      }
+});
   } else {
       errPhone.textContent = "✔";
   }
