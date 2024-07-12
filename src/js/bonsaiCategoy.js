aa2e72754beb30103f970d9140a80d22d4a45d4f
linaMoraP <lina.mora@genstudents.org>
2024-07-12 16:27:17 -0500

    categoria bonsai modificada

--- src/js/bonsaiCategoy.js
+++ src/js/bonsaiCategoy.js
@@ -1,32 +1,47 @@
 //Lista productos categoria plantas bonsai
-
 let products = [ 
     {
       id: 1,
       name: "Bonsái de Pino ",
       price: 150000,
-      img: "../public/img/bonsaiCategory/bonsaiPino.webp",
+      img: "../public/img/imgCategories/bonsaiCategory/bonsaiPino.webp",
+      img2:"../public/img/imgCategories/bonsaiCategory/bonsaiPino.webp", 
+      img3: "../public/img/imgCategories/bonsaiCategory/hojaBonsaiPino.webp",
+      img4:"../public/img/imgCategories/bonsaiCategory/escritorioBonsaiPino.webp",
+      img5: "../public/img/imgCategories/bonsaiCategory/arribaBonsaiPino.webp",
       description: "Pequeño árbol de pino cultivado en maceta."
     },
     {
       id: 2,
       name:"Bonsái de Arce Japonés",
       price: 200000,
-      img: "../public/img/bonsaiCategory/bonsaiJapones.webp",
+      img: "../public/img/imgCategories/bonsaiCategory/bonsaiJapones.webp",
+      img2:"../public/img/imgCategories/bonsaiCategory/bonsaiJapones.webp", 
+      img3: "../public/img/imgCategories/bonsaiCategory/hojabonsaiJapones.webp",
+      img4:"../public/img/imgCategories/bonsaiCategory/arribaBonsaiJapones.webp",
+      img5: "../public/img/imgCategories/bonsaiCategory/escritorioBonsaiJapones.webp",
       description: "Árbol de arce con hojas rojas cultivado en maceta."
     },
     {
         id: 3,
         name:"Bonsái de Ficus",
         price:130000,
-        img:"../public/img/bonsaiCategory/bonsaiFicus.webp",
+        img:"../public/img/imgCategories/bonsaiCategory/bonsaiFicus.webp",
+        img2:"../public/img/imgCategories/bonsaiCategory/bonsaiFicus.webp", 
+        img3: "../public/img/imgCategories/bonsaiCategory/hojabonsaiFicus.webp",
+        img4:"../public/img/imgCategories/bonsaiCategory/escritoriobonsaiFicus.webp",
+        img5: "../public/img/imgCategories/bonsaiCategory/bonsaiFicus.webp",
         description: "Planta aromática utilizada para problemas digestivos."
     },
     {
         id: 4,
         name:"Bonsái de Olmo Chino",
         price: 170000,
-        img: "../public/img/bonsaiCategory/bonsaiOlmoChino.webp",
+        img:"../public/img/imgCategories/bonsaiCategory/bonsaiOlmoChino.webp",
+        img2:"../public/img/imgCategories/bonsaiCategory/bonsaiOlmoChino.webp", 
+        img3: "../public/img/imgCategories/bonsaiCategory/bonsaiOlmoChino.webp",
+        img4:"../public/img/imgCategories/bonsaiCategory/bonsaiOlmoChino.webp",
+        img5: "../public/img/imgCategories/bonsaiCategory/bonsaiFicus.webp",
         description: "Planta aromática con propiedades antiinflamatorias."
     },
     {
@@ -78,16 +93,41 @@ let bonCategoryHTML = '';
 
 for (let i = 0; i < products.length; i++) {
     bonCategoryHTML += `
-      <div class="plant-card">
-        <img src="${products[i].img}" alt="${products[i].name}" class="plantImg">
-        <h2>${products[i].name}</h2>
-        <p>Precio: $${products[i].price}</p>
-        <div class="car">
-        <img src="../public/img/greenCar.webp" alt="car" class="carImg">
+        <div class="plant-card">
+            <div>
+                <div class="container">
+                    <div class="imgContainer">
+                        <img src= "${products[i].img}" alt="" class="mainIMG">
+                    </div>
+                    <div class="xContainer">
+                        <img src="${products[i].img2}" alt="" class="xContainerx active">
+                        <img src="${products[i].img3}" alt="" class="xContainerx">
+                        <img src="${products[i].img4}" alt="" class="xContainerx">
+                        <img src="${products[i].img5}" alt="" class="xContainerx">
+                    </div>
+                </div>
+            </div>
+            <h2>${products[i].name}</h2>
+            <p>Precio: $${products[i].price}</p>
+            <div class="car">
+                <img src="../public/img/greenCar.webp" alt="car" class="carImg">
+            </div>
         </div>
-      </div>
-      `;
+    `;
 }
-    container.innerHTML = bonCategoryHTML;
-    // <p>Descripción: ${products[i].description}</p>
 
+container.innerHTML = bonCategoryHTML;
+
+document.querySelectorAll('.plant-card').forEach(card => {
+    const mainIMG = card.querySelector('.mainIMG');
+    const xContainerx = card.querySelectorAll('.xContainerx');
+
+    xContainerx.forEach(thumb => {
+        thumb.addEventListener('click', function() {
+            const active = card.querySelector('.active');
+            active.classList.remove('active');
+            thumb.classList.add('active');
+            mainIMG.src = thumb.src;
+        });
+    });
+});
