6139f027dff5ab57ffeb5085171a81ea13db702d
krlaNicol <karla.gonzalez@genstudents.org>
2024-07-12 00:08:43 -0500

    bannerNew

--- src/js/sliderNews.js
+++ src/js/sliderNews.js
@@ -1,6 +1,6 @@
 const galleryContainer = document.querySelector('.gallery-container');
 const galleryControlsContainer = document.querySelector('.gallery-controls');
-const galleryControls = ['previous', 'next'];
+const galleryControls = ['<', '>'];
 const galleryItems = document.querySelectorAll('.gallery-item');
 const galleryNavItems = document.querySelectorAll('.gallery-nav li');
 
