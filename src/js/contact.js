f216e5c804f64f47bb260ee549aa8e5b18808fe8
Mateo Rueda <mateo.rueda@genstudents.org>
2024-07-15 05:14:53 -0500

    Modificando footer

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,42 +1,41 @@
-const formulario = document.getElementById('form');
-const nombre = document.getElementById('name');
-const errName = document.getElementById('nameError');
-const phone = document.getElementById('phone');
-const errPhone = document.getElementById('namePhone')
-   // resetear inputs
+const formulario = document.getElementById("form");
+const nombre = document.getElementById("name");
+const errName = document.getElementById("nameError");
+const phone = document.getElementById("phone");
+const errPhone = document.getElementById("namePhone");
+// resetear inputs
 
-nombre.addEventListener('change', (event)=>{
-  longitudInput(event)
-  tipoDato(event)
-})
-function longitudInput(event){
+nombre.addEventListener("change", (event) => {
+  longitudInput(event);
+  tipoDato(event);
+});
+function longitudInput(event) {
   let valorInput = event.target.value;
-let longitudInputNombre = valorInput.length;
-  if(longitudInputNombre < 3){
+  let longitudInputNombre = valorInput.length;
+  if (longitudInputNombre < 3) {
     Swal.fire({
-      title: "Ingrese mas caracteres",
+      title: "Ingresa mas caracteres",
       showConfirmButton: false,
-      background: '#005c39',
-      icon: 'info',
-      timer: '1000',
-      position: 'center',
+      background: "#005c39",
+      icon: "info",
+      timer: "1000",
+      position: "center",
       backdrop: true,
-      color: '#939535',
+      color: "#939535",
       allowOutsideClick: true,
       toost: true,
       showConfirmButton: false,
-      customClass:{
-          title: 'titulo-password'
-      }
-});
-// errName.textContent = ("Ingresa mas caracteres")
-  }else{
-    errName.textContent = ("✔")
+      customClass: {
+        title: "titulo-password",
+      },
+    });
+    // errName.textContent = ("Ingresa mas caracteres")
+  } else {
+    errName.textContent = "✔";
   }
-
 }
 
-phone.addEventListener('change', (event) => {
+phone.addEventListener("change", (event) => {
   tipoDato(event);
 });
 
@@ -46,22 +45,20 @@ function tipoDato(event) {
     Swal.fire({
       title: "Ingrese campo valido",
       showConfirmButton: false,
-      background: '#52c096',
-      icon: 'warning',
-      timer: '7000',
-      position: 'center',
+      background: "#52c096",
+      icon: "warning",
+      timer: "3000",
+      position: "center",
       backdrop: true,
-      color: '#939535',
+      color: "#939535",
       allowOutsideClick: true,
       toost: true,
       showConfirmButton: false,
-      customClass:{
-          title: 'titulo-password'
-      }
-});
+      customClass: {
+        title: "titulo-password",
+      },
+    });
   } else {
-      errPhone.textContent = "✔";
+    errPhone.textContent = "✔";
   }
 }
-
-
